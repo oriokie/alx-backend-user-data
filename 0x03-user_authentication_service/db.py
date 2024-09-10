b44e44874e88b8d1828b8544cc91c6ae14c92dd3
Edwin <oriokie@icloud.com>
2024-09-10 11:40:47 +0300

    edited the add user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,14 +31,10 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a new user to the database"""
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self.session.rollback()
-            new_user = None
-        return new_user
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return (new_user)
 
     def find_user_by(self, **kwargs) -> User:
         """Find a user based on a set of criteria"""
